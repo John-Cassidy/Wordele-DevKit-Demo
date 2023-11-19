c2cb8e349d946d122324bb89632ff7625c655699
John Cassidy <jpcassidy@outlook.com>
2023-11-19 11:07:27 -0500

    modify unit tests to create new TestContext for each test

--- WordeleTests/PlayWordeleTests.cs
+++ WordeleTests/PlayWordeleTests.cs
@@ -7,91 +7,122 @@ using RichardSzalay.MockHttp;
 
 namespace WordeleTests;
 
-public class PlayWordeleTests : TestContext
+public class PlayWordeleTests
 {
     [Fact]
     public void AttemptsStartAtOne()
     {
-        var mock = Services.AddMockHttpClient();
-        mock.When("sample-data/codele-word-library.json").RespondJson(new string[2] { "hello", "world" });
-        var cut = RenderComponent<PlayWordele>();
+        // Arrange
+        using var ctx = new TestContext();
 
+        // Act
+        var mock = ctx.Services.AddMockHttpClient();
+        mock.When("sample-data/codele-word-library.json").RespondJson(new string[2] { "hello", "world" });
+        var cut = ctx.RenderComponent<PlayWordele>();
+
+        // Assert
         cut.Find("strong").MarkupMatches("<strong>Attempt #: 1</strong>");
     }
 
     [Fact]
     public void SubmittingWrongGuess()
     {
-        var mock = Services.AddMockHttpClient();
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Act
+        var mock = ctx.Services.AddMockHttpClient();
         string[] answers = new string[2] { "hello", "world" };
         mock.When("sample-data/codele-word-library.json").RespondJson(answers);
-        var cut = RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
+        var cut = ctx.RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
 
         cut.Find("input").Change("hello");
 
         cut.Find("button").Click();
 
+        // Assert
         cut.Find("strong").MarkupMatches("<strong>Attempt #: 2</strong>");
     }
 
     [Fact]
     public void SubmittingCorrectGuess()
     {
-        var mock = Services.AddMockHttpClient();
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Act
+        var mock = ctx.Services.AddMockHttpClient();
         string[] answers = new string[2] { "hello", "world" };
         mock.When("sample-data/codele-word-library.json").RespondJson(answers);
-        var cut = RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
+        var cut = ctx.RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
 
         cut.Find("input").Change("world");
 
         cut.Find("button").Click();
 
+        // Assert
         cut.Find("strong").MarkupMatches("<strong>Attempt #: 1</strong>");
     }
 
     [Fact]
     public void GuessIsNotFiveLetters()
     {
-        var mock = Services.AddMockHttpClient();
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Act
+        var mock = ctx.Services.AddMockHttpClient();
         string[] answers = new string[2] { "hello", "world" };
         mock.When("sample-data/codele-word-library.json").RespondJson(answers);
-        var cut = RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "hello"));
+        var cut = ctx.RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "hello"));
 
         cut.Find("input").Change("worlds");
 
         cut.Find("button").Click();
 
         var pTags = cut.FindAll("p");
+
+        // Assert
         pTags[2].MarkupMatches(@"<p style=""color: rgb(197, 3, 3);"">Guess must be 5 characters long</p>");
     }
 
     [Fact]
     public void SubmittingCorrectGuessWithMultipleAnswers()
     {
-        var mock = Services.AddMockHttpClient();
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Act
+        var mock = ctx.Services.AddMockHttpClient();
         string[] answers = new string[3] { "hello", "world", "foo" };
         mock.When("sample-data/codele-word-library.json").RespondJson(answers);
-        var cut = RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
+        var cut = ctx.RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
 
         cut.Find("input").Change("world");
 
         cut.Find("button").Click();
 
+        // Assert
         cut.Find("strong").MarkupMatches("<strong>Attempt #: 1</strong>");
     }
 
     [Fact]
     public void SubmittingWrongGuessWithMultipleAnswers()
     {
-        var mock = Services.AddMockHttpClient();
+        // Arrange
+        using var ctx = new TestContext();
+
+        // Act
+        var mock = ctx.Services.AddMockHttpClient();
         string[] answers = new string[3] { "hello", "world", "foo" };
         mock.When("sample-data/codele-word-library.json").RespondJson(answers);
-        var cut = RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
+        var cut = ctx.RenderComponent<PlayWordele>(parameters => parameters.Add(p => p.answers, answers).Add(p => p.answer, "world"));
 
         cut.Find("input").Change("hello");
 
         cut.Find("button").Click();
 
+        // Assert
         cut.Find("strong").MarkupMatches("<strong>Attempt #: 2</strong>");
     }
 }
